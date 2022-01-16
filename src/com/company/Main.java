d46fdad7093ea998e0c8fad86d585acdb25abccd
Rita Tukach <rtukach@gmail.com>
2022-01-16 21:22:11 +0300

    [master] Hello world;

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            String name = "Rita";
+            System.out.println("Hello from " + name);
+    }
+}
